9f95c6d2b1dee9bbabeeaca407cd1f2a062dc7e4
Sascha G <sushi.genji@hotmail.de>
2011-08-13 21:34:55 +0200

    initial commit

---
+++ Gemfile
@@ -0,0 +1,6 @@
+source 'http://rubygems.org'
+
+gem 'rails', '3.0.7'
+gem 'sqlite3-ruby', '1.3.2', :require => 'sqlite3'
+gem "devise", "1.4.2"
+gem "friendly_id", "3.2.1.1"
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Dvdverleih::Application.config.secret_token = 'b8e2908af2e53e8d7302a0e5bd56b4e3a9463a42cadacd0a25d4841974d40e000cbb8c6e473a3aacb0090e31105d65b4fd74c346c6b2f3426e9d18abeba20c9b'
