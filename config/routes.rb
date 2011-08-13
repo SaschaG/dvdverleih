4b20874baf9c9b8a4fa2e06142552c087a22046d
Sascha G <sushi.genji@hotmail.de>
2011-08-13 21:55:12 +0200

    Nutzer wurden angelegt

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,9 @@
 Dvdverleih::Application.routes.draw do
+ devise_for :users
+ resources :users, :only => [:index, :show]
+
+  root :to => "pages#home"
+
   # The priority is based upon order of creation:
   # first created -> highest priority.
 
